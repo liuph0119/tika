5a622750839e35ec07f0cf74e284dd48a13a13e9
liuph0119 <1490569122@qq.com>
2023-05-04 23:49:24 +0800

    init commit

---
+++ test.py
@@ -0,0 +1,9 @@
+# import imp
+# from PIL import Image
+
+# # 1740 x 3095 -> 2302 x 4096
+# # 1740 x 3840 -> 1856 x 4096
+# img = Image.open("/users/liupenghua/documents/41670774938_.pic_hd.jpg")
+# print(img.size, img.format)
+# img = img.resize((1856, 4096), Image.BICUBIC)
+# img.save("/users/liupenghua/documents/41670774938_.pic_hd_large.jpg", quality=100)
